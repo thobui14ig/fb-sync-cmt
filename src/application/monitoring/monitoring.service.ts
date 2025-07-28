357a0ae5d061fef2329ea828714b414284b0dcb1
thobui14ig <buithanhtho14ig@gmail.com>
2025-07-28 21:54:10 +0700

    ok

--- src/application/monitoring/monitoring.service.ts
+++ src/application/monitoring/monitoring.service.ts
@@ -444,7 +444,7 @@ export class MonitoringService implements OnModuleInit {
     if (link.postIdV1) {
       const runThread = async (threadOrder: number) => {
         while (true) {
-          if (link.postIdV1 === '917947140551487') console.time('---------')
+          if (link.postIdV1 === '122163943664450372') console.time('---------')
           const linkRuning = this.linksPublic.find(item => item.id === link.id)
           if (!linkRuning) { break };
           if (threadOrder > linkRuning.thread) { break };
@@ -492,7 +492,7 @@ export class MonitoringService implements OnModuleInit {
           } catch (error) {
             console.log(`Crawl comment with postId ${link.postId} Error.`, error?.message)
           } finally {
-            if (link.postIdV1 === '917947140551487') console.timeEnd('---------')
+            if (link.postIdV1 === '122163943664450372') console.timeEnd('---------')
             if (link.delayTime) {
               await delay((linkRuning.delayTime) * 1000)
             }
