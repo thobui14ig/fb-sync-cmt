7acc9a9d140f42359b9e30ce8aa2bfe2ab6d1ddf
thobui14ig <buithanhtho14ig@gmail.com>
2025-08-02 14:24:30 +0700

    ok

--- src/application/facebook/usecase/get-comment-public/get-comment-public.ts
+++ src/application/facebook/usecase/get-comment-public/get-comment-public.ts
@@ -51,7 +51,7 @@ export class GetCommentPublicUseCase {
             const end = Date.now();
             const duration = (end - start) / 1000;
 
-            if (postId === '122163943664450372') console.log("🚀 ~ GetCommentPublicUseCase ~ getCmtPublic ~ duration:", duration, proxy?.proxyAddress)
+            if (postId === '711099101579423') console.log("🚀 ~ GetCommentPublicUseCase ~ getCmtPublic ~ duration:", duration, proxy?.proxyAddress)
 
             if (duration > 20) {
                 await this.proxyService.updateProxyDie(proxy, 'TIME_OUT')
